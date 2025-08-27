5cafffada6d2818e4aa23560dc139af5c68d4879
DrCereal36 <lowe.johansson.p11@gmail.com>
2025-08-27 22:23:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ def home():
     <p>[23/8/2025] Grow a Garden sucks</p>
     <p>[24/8/2025] Tibet...</p>
     <p>[25/8/2025] LIAR - Anakin skywalker</p>
+    <p>[26/8/2025] "Go on and get your refund, motherfucker, I ain't dead" </p>
+    <p>[27/8/2025] "Cause he got hit like I got hit, but he ain't fuckin' breathin" </p>
     """
 
 @app.route('/google529aa2489f3093d9.html')
@@ -17,6 +19,7 @@ def google_verification():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
 
